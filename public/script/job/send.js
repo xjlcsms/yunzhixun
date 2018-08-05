78a80a90303c773c18397b0850d8979b8730511e
hzr <hzr19960121@163.com>
2018-08-05 19:52:08 +0800

    update

--- public/script/job/send.js
+++ public/script/job/send.js
@@ -1,5 +1,5 @@
 (function(){
-  $('input[name="title"]').bind('input propertychange', function() { 
+  $('input[name="sign"]').bind('input propertychange', function() { 
     var val = $(this).val();
     var content = $('#content').val();
     var end = 0;
@@ -19,20 +19,93 @@
     showLen(content);
   });
 
+  // 字数统计
   $('textarea[name="content"]').bind('input propertychange', function() { 
     var content = $(this).val()
     showLen(content);
   });
 
+  // 导入手机方式选择
   $('input[type="radio"]').click(function(){
     if ($(this).val() == 1) {
       $('#auto').removeClass('none');
       $('#manual').addClass('none');
     } else {
       $('#auto').addClass('none');
-      $('#manual').removeClass('none');
+      $('#manual').removeClass('none'); 
+      $('#smsFile').val('');
+      $('#result').addClass('none');
     }
   });
+
+  // 文件上传
+  $('#smsFile').change(function() {
+    var formdata = new FormData();
+    var smsfile = $("#smsFile")[0].files[0];
+    var idx = window.location.href.indexOf('?id=')
+    var taskid =  window.location.href.substring(idx+4);
+
+    formdata.append("smsfile",smsfile);
+    $.ajax({
+      url: '/index/job/smsfile?taskid=' + taskid,
+      type: "post",
+      data: formdata,
+      async: true,
+      cache: false,
+      contentType: false,
+      processData: false,
+      success: function(res){
+        if (res.status === true) {
+          $('#fileName').text(res.data.filename);
+          $('#totalNum').text(res.data.total);
+          $('#useNum').text(res.data.true);
+          $('#errNum').text(res.data.repeat);
+          $('#reNum').text(res.data.repeat);
+          $('#auto').addClass('none');
+          $('#result').removeClass('none');
+        } else {
+          alert(res.msg)
+        }
+      },
+      error: function(err){
+        console.log(err);
+      }
+    });
+  });
+
+  // 删除文件
+  $('#delSmsFile').click(function() {
+    var params = {
+      fileName: $('#fileName').text()
+    }
+    $.post('/index/job/delsmsf', params, function(res) {
+      if (res.status === true) {
+        $('#smsFile').val('');
+        $('#result').addClass('none');
+        $('#auto').removeClass('none');
+      } else {
+        alert(res.msg);
+      }
+    })
+  });
+
+  // 短信发送
+  $('#sendBtn').click(function() {
+    var idx = window.location.href.indexOf('?id=')
+    var taskid =  window.location.href.substring(idx+4);
+    var params = {
+      type: $('#type').val(),
+      smstype: $('input[name=smstype]:checked').val(),
+      taskid: taskid,
+      content: $('#content').val(),
+      smsfile: $('#fileName').text(),
+      mobiles: $('#phoneText').val()
+    }
+    console.log(params)
+    $.post('/index/job/sms', params, function(res) {
+      console.log(res)
+    })
+  })
 })()
 
 function showLen(content) {
